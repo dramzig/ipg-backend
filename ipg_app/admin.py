69ba59d44ec7d100a5489404ea1f3878d8d90a11
Donaldo Amaya <damaya@crayonstar.com>
2018-05-10 04:19:57 -0300

    se crea modelo profile

--- ipg_app/admin.py
+++ ipg_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django import forms
+from .models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["birth_date", "phone_number"]
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+#class UserAdmin(admin.ModelAdmin):
+#    list_display = ["username", "email"]
+#    class Meta:
+#        model = User
 
 # Register your models here.
+admin.site.register(Profile, ProfileAdmin)
+#admin.site.register(User, UserAdmin)
--- ipg_app/models.py
+++ ipg_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+#from django.contrib.auth.models import UserAttributeSimilarityValidator
 
-# Create your models here.
+class Profile(models.Model):
+    user =models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
